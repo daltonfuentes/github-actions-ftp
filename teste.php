2f4070c8464b4b3a3c43626c192b015aac41819f
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-09 20:19:26 -0300

    n

--- teste.php
+++ teste.php
@@ -50,6 +50,9 @@ if (isset($_POST['polling']) && $_POST['polling'] == true) :
  
     $merchantId = '86c364e5-aa30-499e-aeb1-a2d3ddfc2b3e';
 
+    $outToken = accessToken();
+    $accessToken = $outToken['accessToken'];
+
     $retorno = polling($merchantId); //decode
     $polling = $retorno['polling'];
 
@@ -74,9 +77,9 @@ if (isset($_POST['polling']) && $_POST['polling'] == true) :
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => $in,
+                CURLOPT_POSTFIELDS => array($in),
                 CURLOPT_HTTPHEADER => array(
-                    'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIyYzk5ZWQ0OS00NzhiLTQ5NTktYjM5Mi00ODgyOGVkYTk5NTQiLCJhdWQiOlsic2hpcHBpbmciLCJjYXRhbG9nIiwiZmluYW5jaWFsIiwicmV2aWV3IiwibWVyY2hhbnQiLCJvcmRlciIsIm9hdXRoLXNlcnZlciJdLCJhcHBfbmFtZSI6ImFkbWluc3dlZXRjb25mZXR0eXRlc3RlYyIsIm93bmVyX25hbWUiOiJhZG1pbnN3ZWV0Y29uZmV0dHkiLCJzY29wZSI6WyJzaGlwcGluZyIsImNhdGFsb2ciLCJyZXZpZXciLCJtZXJjaGFudCIsIm9yZGVyIiwiY29uY2lsaWF0b3IiXSwiaXNzIjoiaUZvb2QiLCJtZXJjaGFudF9zY29wZSI6WyI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y29uY2lsaWF0b3IiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y2F0YWxvZyIsIjg2YzM2NGU1LWFhMzAtNDk5ZS1hZWIxLWEyZDNkZGZjMmIzZTpyZXZpZXciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6c2hpcHBpbmciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6bWVyY2hhbnQiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6b3JkZXIiXSwiZXhwIjoxNjQ5NTUzOTcyLCJpYXQiOjE2NDk1NDMxNzIsImp0aSI6IjJjOTllZDQ5LTQ3OGItNDk1OS1iMzkyLTQ4ODI4ZWRhOTk1NCIsIm1lcmNoYW50X3Njb3BlZCI6dHJ1ZSwiY2xpZW50X2lkIjoiMmM5OWVkNDktNDc4Yi00OTU5LWIzOTItNDg4MjhlZGE5OTU0In0.rfZupPEH0jbEz28GLP41C26WFDyz0CpzwoJi94_IhsiEzJde6tH4KOaBpu1WFi5fZw35PeI3Xr5inCejTKirkqtehBAdY77LJRpP1sKKzYpyes8Bof9IJahxODe6f4oW2ZRLlZB4f_ConKiyG_7RBHwM3-VvKcv_lUXLO-1JMIo',
+                    'Authorization: Bearer '.$accessToken,
                     'Content-Type: application/json'
                 ),
                 ));
