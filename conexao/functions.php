360c2440aebd4574985d65e50b0a6793bf0f783b
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-09 16:42:58 -0300

    f

--- conexao/functions.php
+++ conexao/functions.php
@@ -131,35 +131,55 @@ function merchantStatus($accessToken) {
     endif;
 };
 
+$merchantId = '86c364e5-aa30-499e-aeb1-a2d3ddfc2b3e';
+$teste = polling($merchantId);
+var_dump($teste);
+
+function polling($merchantId){
+    require_once("conexao_hostgator.php");
+
+    $out = array();
+
+    $outToken = accessToken();
+    $accessToken = $outToken['accessToken'];
+
     ////
     // FAZ POLLING
     ////
 
+    $curl = curl_init();
 
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => $merchantApiHost.'/order/v1.0/events:polling',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'x-polling-merchants: 86c364e5-aa30-499e-aeb1-a2d3ddfc2b3e',
+        'Authorization: Bearer '.$accessToken
+    ),
+    ));
 
-$curl = curl_init();
+    $response = curl_exec($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://merchant-api.ifood.com.br/order/v1.0/events:polling',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'x-polling-merchants: 86c364e5-aa30-499e-aeb1-a2d3ddfc2b3e',
-    'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIyYzk5ZWQ0OS00NzhiLTQ5NTktYjM5Mi00ODgyOGVkYTk5NTQiLCJhdWQiOlsic2hpcHBpbmciLCJjYXRhbG9nIiwiZmluYW5jaWFsIiwicmV2aWV3IiwibWVyY2hhbnQiLCJvcmRlciIsIm9hdXRoLXNlcnZlciJdLCJhcHBfbmFtZSI6ImFkbWluc3dlZXRjb25mZXR0eXRlc3RlYyIsIm93bmVyX25hbWUiOiJhZG1pbnN3ZWV0Y29uZmV0dHkiLCJzY29wZSI6WyJzaGlwcGluZyIsImNhdGFsb2ciLCJyZXZpZXciLCJtZXJjaGFudCIsIm9yZGVyIiwiY29uY2lsaWF0b3IiXSwiaXNzIjoiaUZvb2QiLCJtZXJjaGFudF9zY29wZSI6WyI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y29uY2lsaWF0b3IiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y2F0YWxvZyIsIjg2YzM2NGU1LWFhMzAtNDk5ZS1hZWIxLWEyZDNkZGZjMmIzZTpyZXZpZXciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6c2hpcHBpbmciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6bWVyY2hhbnQiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6b3JkZXIiXSwiZXhwIjoxNjQ5NTM2OTg1LCJpYXQiOjE2NDk1MjYxODUsImp0aSI6IjJjOTllZDQ5LTQ3OGItNDk1OS1iMzkyLTQ4ODI4ZWRhOTk1NCIsIm1lcmNoYW50X3Njb3BlZCI6dHJ1ZSwiY2xpZW50X2lkIjoiMmM5OWVkNDktNDc4Yi00OTU5LWIzOTItNDg4MjhlZGE5OTU0In0.evkT65epyYgaqNC_h-EHQsj1DWmIMeeKbI1fcJ3WH5enlDOUMhaR-FVtrNp8_TOP3cGN6_j9kLEltx24yiIWFCU1aSOIup448hczTQ74KHHx57knlZZgSJCGyU0u2BYpQDccsDqRGpu2d79xdcUd9qGT1k3jcXR6xhunI-2Gyi4'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
-
-
+    if($httpcode == 200):
+        $retorno = json_decode($response, true);
+        $out['erro'] = 0;
+        $out['polling'] = $retorno;
+        return $out;
+    else:
+        $out['mensagem'] = 'Erro inesperado.';
+        $out['erro'] = 1;
+        $out['code'] = $httpcode;
+        return $out;
+    endif;
+};
 
 function acknowledgment($send){
     require_once("conexao_hostgator.php");
