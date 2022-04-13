71c55a7a4a73c01910635a36246edff13428389f
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-13 14:41:33 -0300

    type

--- teste.php
+++ teste.php
@@ -43,7 +43,7 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
     endif;
 endif;
 
-//$_POST['polling'] = true;
+$_POST['polling'] = true;
 
 if (isset($_POST['polling']) && $_POST['polling'] == true) :
  
