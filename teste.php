b28b8693d3dcf35a99bb7b8a13dfd1b675b4472d
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-13 12:16:53 -0300

    type

--- teste.php
+++ teste.php
@@ -53,12 +53,14 @@ if (isset($_POST['polling']) && $_POST['polling'] == true) :
     $outToken = accessToken();
     $accessToken = $outToken['accessToken'];
 
+    echo gettype($accessToken);
+    echo $accessToken;
+    exit();
+
     $retorno = polling($merchantId); //decode
     $polling = $retorno['polling'];
 
-    echo gettype($polling);
-
-    exit();
+    
 
     if($retorno['erro'] == 0):
 
