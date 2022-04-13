69b292304085b8b88c80da4a8a9fcdb48ce48b01
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-13 14:06:49 -0300

    teste

--- teste.php
+++ teste.php
@@ -52,11 +52,31 @@ if (isset($_POST['polling']) && $_POST['polling'] == true) :
     $outToken = accessToken();
     $accessToken = $outToken['accessToken'];
 
-    $retorno = polling($merchantId);
-    $polling = $outPolling['polling'];
+    $merchantApiHost = 'https://merchant-api.ifood.com.br';
 
+    $curl = curl_init();
 
-    echo gettype($polling);
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => $merchantApiHost.'/order/v1.0/events:polling',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        "x-polling-merchants: $merchantId",
+        "Authorization: Bearer $accessToken",
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response.'<hr>';
+
+    echo gettype($response);
     exit();
 
 
