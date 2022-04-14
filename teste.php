13949f5d28c79012ceeaa50759ca91f75af8d803
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-14 08:56:55 -0300

    c

--- teste.php
+++ teste.php
@@ -243,6 +243,91 @@ endif;
 
 
 
+function accessToken(){
+    require_once("./conexao/conexao_hostgator.php");
+
+    $out = array();
+
+    $date = new DateTime();
+    $dateComparacao = date_format($date, 'YmdHis');
+
+    $sql = "SELECT accessToken FROM token_ifood WHERE expire>'$dateComparacao'";
+    $resultado = $conexao->prepare($sql);	
+    $resultado->execute();
+    $contar = $resultado->rowCount();
+
+    if($contar == 0):
+        ////
+        // PEGA 'clientId' . 'clientSecret'
+        ////
+
+        $clientId = '2c99ed49-478b-4959-b392-48828eda9954';
+        $clientSecret = 'oqy93k6snxjmgruuabkt82hz7fw5djuorlcxcaaapdtzo0zsa0neyz3ztqv452ein8d3j0jibusrejcox7mzkzoakzmokg93rc7';
+
+        ////
+        // SOLICITA TOKEN DE ACESSO
+        ////
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $merchantApiHost.'/authentication/v1.0/oauth/token',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'grantType=client_credentials&clientId='.$clientId.'&clientSecret='.$clientSecret,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if($httpcode == 200):
+            $retorno = json_decode($response, true);
+            $token = $retorno['accessToken'];
+            $expiresIn = $retorno['expiresIn'];
+            $expiresIn = $expiresIn-60;
+
+            $date = new DateTime();
+            date_add($date, date_interval_create_from_date_string($expiresIn.' second'));
+            $expire = date_format($date, 'YmdHis');
+
+            $sql = 'INSERT INTO token_ifood (accessToken, expire) VALUES (:accessToken, :expire)';
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindParam(':accessToken', $token);
+            $stmt->bindParam(':expire', $expire);
+            $resposta = $stmt->execute();
+
+            if(!$resposta):
+                $out['mensagem'] = 'Erro inesperado.';
+                $out['erro'] = 600;
+                $out['accessToken'] = 0;
+                return $out;
+            else:
+                $out['erro'] = 0;
+                $out['accessToken'] = $retorno['accessToken'];
+                return $out;
+            endif;
+        else:
+            $out['mensagem'] = 'Erro inesperado.';
+            $out['erro'] = 1;
+            $out['code'] = $httpcode;
+            return $out;
+        endif;
+    else:
+        $exibe = $resultado->fetch(PDO::FETCH_OBJ);
+        $out['erro'] = 0;
+        $out['accessToken'] = $exibe->accessToken;
+        return $out;
+    endif;
+};
+
 
 
 
