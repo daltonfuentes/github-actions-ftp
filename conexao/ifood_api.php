697fd130a8fde829c099932f86ce046d657e5d53
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-26 06:18:07 -0300

    t

--- conexao/ifood_api.php
+++ conexao/ifood_api.php
@@ -49,8 +49,9 @@ if (isset($_POST['polling']) && $_POST['polling'] == true) :
 
     $merchantId = '86c364e5-aa30-499e-aeb1-a2d3ddfc2b3e';
 
-    $outToken = accessToken();
-    $accessToken = $outToken['accessToken'];
+    //$outToken = accessToken();
+    //$accessToken = $outToken['accessToken'];
+    $accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIyYzk5ZWQ0OS00NzhiLTQ5NTktYjM5Mi00ODgyOGVkYTk5NTQiLCJhdWQiOlsic2hpcHBpbmciLCJjYXRhbG9nIiwiZmluYW5jaWFsIiwicmV2aWV3IiwibWVyY2hhbnQiLCJvcmRlciIsIm9hdXRoLXNlcnZlciJdLCJhcHBfbmFtZSI6ImFkbWluc3dlZXRjb25mZXR0eXRlc3RlYyIsIm93bmVyX25hbWUiOiJhZG1pbnN3ZWV0Y29uZmV0dHkiLCJzY29wZSI6WyJzaGlwcGluZyIsImNhdGFsb2ciLCJyZXZpZXciLCJtZXJjaGFudCIsIm9yZGVyIiwiY29uY2lsaWF0b3IiXSwiaXNzIjoiaUZvb2QiLCJtZXJjaGFudF9zY29wZSI6WyI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y29uY2lsaWF0b3IiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6Y2F0YWxvZyIsIjg2YzM2NGU1LWFhMzAtNDk5ZS1hZWIxLWEyZDNkZGZjMmIzZTpyZXZpZXciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6c2hpcHBpbmciLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6bWVyY2hhbnQiLCI4NmMzNjRlNS1hYTMwLTQ5OWUtYWViMS1hMmQzZGRmYzJiM2U6b3JkZXIiXSwiZXhwIjoxNjUwOTc1NDQwLCJpYXQiOjE2NTA5NjQ2NDAsImp0aSI6IjJjOTllZDQ5LTQ3OGItNDk1OS1iMzkyLTQ4ODI4ZWRhOTk1NCIsIm1lcmNoYW50X3Njb3BlZCI6dHJ1ZSwiY2xpZW50X2lkIjoiMmM5OWVkNDktNDc4Yi00OTU5LWIzOTItNDg4MjhlZGE5OTU0In0.Apt5AK6SXgc4ZbbJ7GIOWpdiA23FjA1A0wPlkjjcHcUewf7F8MGfNo69_gy00hmRE8h4HBmpf_lx5xfB3AsVVN5lWv9wcT_jdoMPyKeVpstmoMwXJ2Bc02IyrhjZ_MGfxVoTmMK8ZOoqwom2vA3pkT4GjmakWUxaD_fEsCa21NI';
 
     $outPolling = polling($merchantId, $accessToken);
     $polling = $outPolling['polling']; //CHEGA COMO ARRAY
