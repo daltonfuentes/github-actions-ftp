e64f6e892e5c4f417e7f9191218cef85828efb9c
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-28 06:30:14 -0300

    t

--- conexao/ifood_api.php
+++ conexao/ifood_api.php
@@ -23,6 +23,9 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
         exit();
     endif;
 
+    $validations = (array) $outDetails['validations'];
+    $validation1 = (array) $validations[1];
+
     if($state == 'CLOSED' || $state == 'ERROR'):
         // LOJA FECHADA
         $retorno['title']  = $outState['title'];
@@ -33,7 +36,7 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
     elseif($state == 'OK' || $state == 'WARNING'):
         // LOJA ABERTA
         $retorno['title']  = $outState['title'];
-        $retorno['subtitle']  = $outState['validations'][1]['code'];
+        $retorno['subtitle']  = $validation1['code'];
         $retorno['code']  = $outState['code'];
         echo json_encode($retorno);
         exit();
