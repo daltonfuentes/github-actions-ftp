6df4c2ab7d17ac9363df7dd1e68148415ec581e0
daltonfuentes <daltonfuentes2020@gmail.com>
2022-04-28 09:16:03 -0300

    t

--- conexao/ifood_api.php
+++ conexao/ifood_api.php
@@ -30,7 +30,7 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
         $retorno['merchantStatus']  = 'open';
 
         $html = '
-        <div class="dropdown-menu dropdown-menu-status p-3" style="width: 350px;">
+        <div class="dropdown-menu-dalton dropdown-menu-status p-3" style="width: 350px;">
             <h4 class="fs-16 font-w600 text-black mb-0">Loja aberta<i class="fa-solid fa-circle-check text-success ml-2"></i></h4>
             <hr class="">
             <h4 class="fs-14 font-w600 text-black pb-2"><i class="fa-solid fa-check text-success mr-2"></i>'.$outState['validations']['is-connected']['message']['title'].'</h4>
@@ -49,7 +49,7 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
         $retorno['merchantStatus']  = 'open';
 
         $html = '
-        <div class="dropdown-menu dropdown-menu-status p-3" style="width: 350px;">
+        <div class="dropdown-menu-dalton dropdown-menu-status p-3" style="width: 350px;">
             <h4 class="fs-16 font-w600 text-black mb-0">Loja aberta<i class="fa-solid fa-circle-check text-success ml-2"></i></h4>
             <hr class="">
             <h4 class="fs-14 font-w600 text-black p-3 bg-observation-order" style="margin-left: -16px;margin-right: -16px;"><i class="fa-regular fa-clock text-black mr-2 fs-16"></i>'.$outState['validations']['radius-restriction']['message']['title'].'  <br><span class="fs-12 font-w400 ml-4">'.$outState['validations']['radius-restriction']['message']['subtitle'].'</span><br><span class="fs-12 font-w400 ml-4">Muitos pedidos (alterado no Gestor de Pedidos)</span></h4>
@@ -112,7 +112,7 @@ if (isset($_POST['status_ifood']) && $_POST['status_ifood'] == true) :
         endif;
 
         $html = '
-        <div class="dropdown-menu dropdown-menu-status p-3" style="width: 350px;">
+        <div class="dropdown-menu-dalton dropdown-menu-status p-3" style="width: 350px;">
             <h4 class="fs-16 font-w600 text-black mb-0">Loja fechada <i class="fa-solid fa-ban text-black ml-1"></i><br></h4>
             <hr class="">'.
             $linhaUnavailabilities.$linhaRadiusRestriction.$linhaPayoutBlocked.$linhaLogisticsBlocked.$linhaTermsServiceViolation.$linhaStatusAvailability
