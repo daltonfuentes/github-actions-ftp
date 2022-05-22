2f92f17d01aacea677b6b598e5fd5403153d0baa
daltonfuentes <daltonfuentes2020@gmail.com>
2022-05-22 02:08:51 -0300

    P

--- conexao/functions.php
+++ conexao/functions.php
@@ -280,6 +280,7 @@ function orderConfirm($orderId, $accessToken){
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'grantType=client_credentials',
     CURLOPT_HTTPHEADER => array(
         "Authorization: Bearer $accessToken"
     ),
