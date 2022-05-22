bac77fe0926a3fd1a128e988417bbe5a89fe93b9
daltonfuentes <daltonfuentes2020@gmail.com>
2022-05-22 02:56:22 -0300

    dsp

--- js/painel-pedidos.js
+++ js/painel-pedidos.js
@@ -277,8 +277,56 @@ $(window).on("load", function(){
 
     $(document).on('click', '.btnOrderDsp', function(){
         var orderId = $(this).attr('data-orderId');
+        var origin = $(this);
 
+        if(origin.hasClass('disabled') == true){
+            return;
+        }else{
+            origin.html('<i class="fa-duotone fa-spinner-third fs-18 fa-spin"></i>');
+            origin.addClass('disabled');
+        }
 
+        $.ajax({
+            type : 'POST',
+            url  : './conexao/ifood_api.php',
+            data : { order_ifood_dsp: true, orderId: orderId },
+            dataType: 'json',
+            beforeSend: function() {
+                
+            },
+            success :  function(retorno){
+                if(retorno.error == false){
+                    $.ajax({
+                        type : 'POST',
+                        url  : './conexao/ifood_api.php',
+                        data : { polling: true },
+                        dataType: 'json',
+                        beforeSend: function() {
+                            
+                        },
+                        success :  function(retorno){
+                            
+                        },
+                        error: function() {
+                            
+                        },
+                        complete: function() {
+                            listOrders();
+                        }
+                    });
+                }else{
+                    origin.html('<i class="fa-solid fa-motorcycle fs-16"></i> <span class="ml-2 fs-16">DESPACHAR</span>');
+                    origin.removeClass('disabled');
+                }          
+            },
+            error: function() {
+                origin.html('<i class="fa-solid fa-motorcycle fs-16"></i> <span class="ml-2 fs-16">DESPACHAR</span>');
+                origin.removeClass('disabled');
+            },
+            complete: function() {
+    
+            }
+        });
     });
 
     $(document).on('click', '.btnOrderCan', function(){
